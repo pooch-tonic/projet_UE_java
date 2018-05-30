b5a30bd0a1020bc3dee09261430afe51aa42c386
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-30 22:44:51 +0200

    corrected IVectorFactory import

--- Lorann/contract/src/main/java/model/IVectorFactory.java
+++ Lorann/contract/src/main/java/model/IVectorFactory.java
@@ -1,9 +1,7 @@
 package model;
 
-import java.util.Vector;
-
 import enums.DirectionsEnum;
 
 public interface IVectorFactory {
-	public Vector getVectorForDirection(final DirectionsEnum direction);
+	public IVector getVectorForDirection(final DirectionsEnum direction);
 }
