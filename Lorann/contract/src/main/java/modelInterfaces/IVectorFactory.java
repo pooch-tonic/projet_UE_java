0504975519ad92963927d2eb11d8223c4ac24958
PtitCaillou <33094458+PtitCaillou@users.noreply.github.com>
2018-05-31 13:31:31 +0200

    Vector factory and DirectionsEnum done !

--- Lorann/contract/src/main/java/modelInterfaces/IVectorFactory.java
+++ Lorann/contract/src/main/java/modelInterfaces/IVectorFactory.java
@@ -1,7 +1,4 @@
 package modelInterfaces;
 
-import enums.DirectionsEnum;
-
 public interface IVectorFactory {
-	public IVector getVectorForDirection(final DirectionsEnum direction);
 }
--- Lorann/model/src/main/java/model/factories/VectorFactory.java
+++ Lorann/model/src/main/java/model/factories/VectorFactory.java
@@ -1,7 +1,70 @@
 package model.factories;
 
+import model.vector.Vector;
 import modelInterfaces.IVectorFactory;
 
-public abstract class VectorFactory implements IVectorFactory{
+abstract class VectorFactory implements IVectorFactory{
+	private static Vector UPLEFT = new Vector(-1, -1);
+	private static Vector UP = new Vector(0, -1);
+	private static Vector UPRIGHT = new Vector(1, -1);
+	private static Vector RIGHT = new Vector(1, 0);
+	private static Vector DOWNRIGHT = new Vector(1, 1);
+	private static Vector DOWN = new Vector(0,1);
+	private static Vector DOWNLEFT = new Vector(-1, 1);
+	private static Vector LEFT = new Vector(-1, 0);
+	private static Vector MIDDLE = new Vector(0, 0);
+	
+	public static Vector getUPLEFT() {
+		return UPLEFT;
+	}
+	public static Vector getUP() {
+		return UP;
+	}
+	public static Vector getUPRIGHT() {
+		return UPRIGHT;
+	}
+	public static Vector getRIGHT() {
+		return RIGHT;
+	}
+	public static Vector getDOWNRIGHT() {
+		return DOWNRIGHT;
+	}
+	public static Vector getDOWN() {
+		return DOWN;
+	}
+	public static Vector getDOWNLEFT() {
+		return DOWNLEFT;
+	}
+	public static Vector getLEFT() {
+		return LEFT;
+	}
+	public static Vector getMIDDLE() {
+		return MIDDLE;
+	}
 
+	public static Vector getVectorForDirection(DirectionsEnum direction) {
+		switch(direction) {
+			case UPLEFT :
+				return getUPLEFT();
+			case UP :
+				return getUP();
+			case UPRIGHT :
+				return getUPRIGHT();
+			case RIGHT :
+				return getRIGHT();
+			case DOWNRIGHT :
+				return getDOWNRIGHT();
+			case DOWN :
+				return getDOWN();
+			case DOWNLEFT :
+				return getDOWNLEFT();
+			case LEFT :
+				return getLEFT();
+			case MIDDLE :
+				return getMIDDLE();
+			default:
+				break;
+		}
+		return null;
+	}
 }
