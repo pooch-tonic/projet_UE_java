4c172302af4faf45dc5c695bcca3d3557c9ec400
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-06-01 09:58:41 +0200

    movement strategy tweaks

---
+++ Lorann/model/src/main/java/model/moveStrategy/BounceTowardsPlayer.java
@@ -0,0 +1,14 @@
+package model.moveStrategy;
+
+import modelInterfaces.IMoveStrategy;
+
+public class BounceTowardsPlayer implements IMoveStrategy {
+
+	/**
+	 * strategy which makes the unit bounce towards the player if possible
+	 */
+	@Override
+	public void move() {
+		// TODO move strategy #2
+	}
+}
