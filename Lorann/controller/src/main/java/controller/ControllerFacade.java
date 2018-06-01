4ecdd90de4d95b7ce734db7491385716c5b2eaf9
PtitCaillou <33094458+PtitCaillou@users.noreply.github.com>
2018-06-01 09:44:13 +0200

    removed example query from ControllerFacade

--- Lorann/controller/src/main/java/controller/ControllerFacade.java
+++ Lorann/controller/src/main/java/controller/ControllerFacade.java
@@ -45,19 +45,19 @@ public class ControllerFacade implements IController, IOrderStacker {
      *             the SQL exception
      */
     public void start() throws SQLException {
-        this.getView()
-                .displayMessage(this.getModel().getExampleById(1).toString());
-
-        this.getView().displayMessage(
-                this.getModel().getExampleByName("Example 2").toString());
-
-        final List<Example> examples = this.getModel().getAllExamples();
-        final StringBuilder message = new StringBuilder();
-        for (final Example example : examples) {
-            message.append(example);
-            message.append('\n');
-        }
-        this.getView().displayMessage(message.toString());
+//        this.getView()
+//                .displayMessage(this.getModel().getExampleById(1).toString());
+//
+//        this.getView().displayMessage(
+//                this.getModel().getExampleByName("Example 2").toString());
+//
+//        final List<Example> examples = this.getModel().getAllExamples();
+//        final StringBuilder message = new StringBuilder();
+//        for (final Example example : examples) {
+//            message.append(example);
+//            message.append('\n');
+//        }
+//        this.getView().displayMessage(message.toString());
     }
 
     /**
