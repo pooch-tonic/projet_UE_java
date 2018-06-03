62052054a9da409513d8ecc87d19219daf1ae7d4
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-06-03 14:47:41 +0200

    Prepared all strategies

---
+++ Lorann/model/src/main/java/model/behaviorStrategy/MoveSimple.java
@@ -0,0 +1,17 @@
+package model.behaviorStrategy;
+
+import modelInterfaces.IMoveStrategy;
+
+public class MoveSimple implements IMoveStrategy {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see modelInterfaces.IMoveStrategy#move()
+	 */
+	@Override
+	public void move() {
+
+	}
+
+}
