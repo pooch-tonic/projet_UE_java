ee47a20617ba61d5ff5d4faefdda8753aaa63d2e
OrelRKM <33094417+OrelRKM@users.noreply.github.com>
2018-06-04 22:48:40 +0200

    some changes

--- Lorann/model/src/main/java/model/behaviorStrategy/BounceTowardsPlayer.java
+++ Lorann/model/src/main/java/model/behaviorStrategy/BounceTowardsPlayer.java
@@ -11,26 +11,27 @@ import vector.IVector;
 
 public class BounceTowardsPlayer implements IBounceStrategy {
 
-	/**
-	 * strategy which makes the unit bounce towards the player if possible
-	 *
-	 * @see modelInterfaces.IBounceStrategy#bounce()
-	 */
-	@Override
-	public void bounce(final IEntity actor, final ILevel level) {
-		final IVector actorPosition = actor.getPosition();
-		final IVector playerPosition = level.getPlayer().getPosition();
-		final IVector chasingDirection = playerPosition.getSubResult(actorPosition);
-		chasingDirection.normalize();
-		final IVector actorNextPosition = actorPosition.getAddResult(chasingDirection);
+    /**
+     * strategy which makes the unit bounce towards the player if possible
+     *
+     * @see modelInterfaces.IBounceStrategy#bounce()
+     */
+    @Override
+    public void bounce(final IEntity actor, final ILevel level) {
+        final IVector actorPosition = actor.getPosition();
+        final IVector playerPosition = level.getPlayer().getPosition();
+        final IVector chasingDirection = playerPosition.getSubResult(actorPosition);
+        chasingDirection.normalize();
+        final IVector actorNextPosition = actorPosition.getAddResult(chasingDirection);
 
-		if (level.getUnits()[actorNextPosition.getX()][actorNextPosition.getY()].getType() != Type.WALL) {
-			actor.setDirection(chasingDirection);
-		} else {
-			// TODO a enrichir
-			actor.getDirection().invert();
-		}
+        if ((level.getUnits()[actorNextPosition.getX()][actorNextPosition.getY()]
+                .getType() != Type.WALL) && (level.getEntityOn(actorNextPosition) == null)) {
+            actor.setDirection(chasingDirection);
+        } else {
+            // TODO a enrichir
+            actor.getDirection().invert();
+        }
 
-	}
+    }
 
 }
