b521be41fe5c84eb2a5b656a6e2840d4610a6b13
OrelRKM <33094417+OrelRKM@users.noreply.github.com>
2018-06-04 13:14:19 +0200

    up

--- Lorann/model/src/main/java/model/behaviorStrategy/BounceTowardsPlayer.java
+++ Lorann/model/src/main/java/model/behaviorStrategy/BounceTowardsPlayer.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package model.behaviorStrategy;
 
 import enums.Type;
@@ -8,28 +11,28 @@ import vector.IVector;
 
 public class BounceTowardsPlayer implements IBounceStrategy {
 
-	/**
-	 * strategy which makes the unit bounce towards the player if possible
-	 *
-	 * @see modelInterfaces.IBounceStrategy#bounce()
-	 */
-	@Override
-	public void bounce(final IEntity actor, final ILevel level) {
-		IVector actorPosition = actor.getPosition();
-		IVector actorDirection = actor.getDirection();
-		IVector playerPosition = level.getPlayer().getPosition();
-		IVector chasingDirection = playerPosition.getSubResult(actorPosition);
-		chasingDirection.normalize();
-		IVector actorNextPosition = actorPosition.getAddResult(chasingDirection);
+    /**
+     * strategy which makes the unit bounce towards the player if possible
+     *
+     * @see modelInterfaces.IBounceStrategy#bounce()
+     */
+    @Override
+    public void bounce(final IEntity actor, final ILevel level) {
+        final IVector actorPosition = actor.getPosition();
+        final IVector actorDirection = actor.getDirection();
+        final IVector playerPosition = level.getPlayer().getPosition();
+        final IVector chasingDirection = playerPosition.getSubResult(actorPosition);
+        chasingDirection.normalize();
+        final IVector actorNextPosition = actorPosition.getAddResult(chasingDirection);
 
-		if (level.getUnits()[actorNextPosition.getX()][actorNextPosition.getY()].getType() != Type.WALL) {
-			actor.setDirection(chasingDirection);
-		} else {
+        if (level.getUnits()[actorNextPosition.getX()][actorNextPosition.getY()]
+                .getType() != Type.WALL) {
+            actor.setDirection(chasingDirection);
+        } else {
 
-		}
+        }
 
-
-		level.getUnits()[][y]
-	}
+        // level.getUnits()[][y]
+    }
 
 }
