fc494f4faf69ffe6b09a91814ac822cf959942d8
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-06-04 18:08:21 +0200

    More tests

--- Lorann/model/test/factoriesTest/VectorFactoryTest.java
+++ Lorann/model/test/factoriesTest/VectorFactoryTest.java
@@ -1,12 +1,15 @@
 package factoriesTest;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import model.factories.VectorFactory;
+import vector.Vector;
 
 public class VectorFactoryTest {
 
@@ -27,8 +30,57 @@ public class VectorFactoryTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDown() {
+		Vector vector = new Vector(0, 1);
+		assertEquals(true, VectorFactory.getDOWN().isEqual(vector));
+	}
+
+	@Test
+	public void testDownLeft() {
+		Vector vector = new Vector(-1, 1);
+		assertEquals(true, VectorFactory.getDOWNLEFT().isEqual(vector));
+	}
+
+	@Test
+	public void testDownRight() {
+		Vector vector = new Vector(1, 1);
+		assertEquals(true, VectorFactory.getDOWNRIGHT().isEqual(vector));
+	}
+
+	@Test
+	public void testLeft() {
+		Vector vector = new Vector(-1, 0);
+		assertEquals(true, VectorFactory.getLEFT().isEqual(vector));
+	}
+
+	@Test
+	public void testMiddle() {
+		Vector vector = new Vector(0, 0);
+		assertEquals(true, VectorFactory.getMIDDLE().isEqual(vector));
+	}
+
+	@Test
+	public void testRight() {
+		Vector vector = new Vector(1, 0);
+		assertEquals(true, VectorFactory.getRIGHT().isEqual(vector));
+	}
+
+	@Test
+	public void testUp() {
+		Vector vector = new Vector(0, -1);
+		assertEquals(true, VectorFactory.getUP().isEqual(vector));
+	}
+
+	@Test
+	public void testUpLeft() {
+		Vector vector = new Vector(-1, -1);
+		assertEquals(true, VectorFactory.getUPLEFT().isEqual(vector));
+	}
+
+	@Test
+	public void testUpRight() {
+		Vector vector = new Vector(1, -1);
+		assertEquals(true, VectorFactory.getUPRIGHT().isEqual(vector));
 	}
 
 }
