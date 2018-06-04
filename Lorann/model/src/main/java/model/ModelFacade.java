188ee10d2f225877c489b97f7fca95eb574a1ac5
OrelRKM <33094417+OrelRKM@users.noreply.github.com>
2018-06-04 10:40:09 +0200

    correctifs

--- Lorann/model/src/main/java/model/ModelFacade.java
+++ Lorann/model/src/main/java/model/ModelFacade.java
@@ -14,6 +14,7 @@ import enums.DirectionEnum;
 import enums.TypeEnum;
 import model.dao.QueryDAO;
 import model.factories.UnitFactory;
+import model.factories.VectorFactory;
 import modelInterfaces.IEntity;
 import modelInterfaces.ILevel;
 import modelInterfaces.IModel;
@@ -27,251 +28,254 @@ import vector.IVector;
  * @version 1.0
  */
 public final class ModelFacade extends Observable implements IModel {
-	private ILevel level;
-	private Score score;
+    private ILevel level;
+    private Score  score;
 
-	private Dimension dimensionTemp; // TODO supprimer
+    private final Dimension dimensionTemp; // TODO supprimer
 
-	/**
-	 * Instantiates a new model facade.
-	 */
-	public ModelFacade(final Dimension dimension) {
-		super();
-		// Creates a ground-only level
-		this.level = new Level(0, dimension);
-		this.dimensionTemp = dimension; // TODO supprimer
+    /**
+     * Instantiates a new model facade.
+     */
+    public ModelFacade(final Dimension dimension) {
+        super();
+        // Creates a ground-only level
+        this.level = new Level(0, dimension);
+        this.dimensionTemp = dimension; // TODO supprimer
 
-		this.fillVoidSquares();
-		this.setScore(new Score());
-	}
+        this.fillVoidSquares();
+        this.setScore(new Score());
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see modelInterfaces.IModel#addToScore(int)
-	 */
-	@Override
-	public void addToScore(final int value) {
-		this.getScore().addToScoreValue(value);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see modelInterfaces.IModel#addToScore(int)
+     */
+    @Override
+    public void addToScore(final int value) {
+        this.getScore().addToScoreValue(value);
+    }
 
-	@Override
-	public void destroyEntity(final IEntity entity) {
-		this.addToScore(entity.getScoreValue());
-		this.getLevel().removeEntityFromLevel(entity);
-	}
+    @Override
+    public void destroyEntity(final IEntity entity) {
+        this.addToScore(entity.getScoreValue());
+        this.getLevel().removeEntityFromLevel(entity);
+    }
 
-	private void fillVoidSquares() {
-		IUnit[][] units = this.getLevel().getUnits();
+    private void fillVoidSquares() {
+        final IUnit[][] units = this.getLevel().getUnits();
 
-		// TODO vérifier que x et y ne sont jamais inversés.
-		for (int x = 0; x < units.length; x++) {
-			for (int y = 0; y < units[0].length; y++) {
-				if (units[x][y] == null) {
-					this.getLevel().addUnit(new Ground(), x, y);
-				}
-			}
-		}
-	}
+        // TODO vérifier que x et y ne sont jamais inversés.
+        for (int x = 0; x < units.length; x++) {
+            for (int y = 0; y < units[0].length; y++) {
+                if (units[x][y] == null) {
+                    this.getLevel().addUnit(new Ground(), x, y);
+                }
+            }
+        }
+    }
 
-	@Override
-	public IEntity getExit() {
-		return this.getLevel().getExit();
-	}
+    @Override
+    public IEntity getExit() {
+        return this.getLevel().getExit();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see modelInterfaces.IModel#getLevel()
-	 */
-	@Override
-	public ILevel getLevel() {
-		// TODO Auto-generated method stub
-		return this.level;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see modelInterfaces.IModel#getLevel()
+     */
+    @Override
+    public ILevel getLevel() {
+        // TODO Auto-generated method stub
+        return this.level;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.IModel#getMap(int)
-	 */
-	@Override
-	public Dimension getMap(final int mapId) throws SQLException {
-		return QueryDAO.getMap(mapId);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.IModel#getMap(int)
+     */
+    @Override
+    public Dimension getMap(final int mapId) throws SQLException {
+        return QueryDAO.getMap(mapId);
+    }
 
-	@Override
-	public IEntity getPlayer() {
-		return this.getLevel().getPlayer();
-	}
+    @Override
+    public IEntity getPlayer() {
+        return this.getLevel().getPlayer();
+    }
 
-	/**
-	 * Gets the score
-	 *
-	 * @return the score
-	 */
-	private Score getScore() {
-		return this.score;
-	}
+    /**
+     * Gets the score
+     *
+     * @return the score
+     */
+    private Score getScore() {
+        return this.score;
+    }
 
-	/**
-	 * Gets the path of the sprite.
-	 *
-	 * @param type
-	 *            the type of the unit
-	 * @return the path of the sprite
-	 * @throws SQLException
-	 *             the SQL exception
-	 */
-	private ArrayList<String> getSpritePath(final TypeEnum type) throws SQLException {
-		final ArrayList<String> result = QueryDAO.getSpritePath(type);
-		return result;
-	}
+    /**
+     * Gets the path of the sprite.
+     *
+     * @param type
+     *            the type of the unit
+     * @return the path of the sprite
+     * @throws SQLException
+     *             the SQL exception
+     */
+    private ArrayList<String> getSpritePath(final TypeEnum type) throws SQLException {
+        final ArrayList<String> result = QueryDAO.getSpritePath(type);
+        return result;
+    }
 
-	/**
-	 * Gets the unit by map.
-	 *
-	 * @param mapId
-	 *            the id of the map
-	 * @return the unit by map
-	 * @throws SQLException
-	 *             the SQL exception
-	 */
-	public HashMap<String, ArrayList<IVector>> getUnitByMap(final int mapId) throws SQLException {
-		final HashMap<String, ArrayList<IVector>> result = QueryDAO.getUnitByMap(mapId);
-		return result;
-	}
+    /**
+     * Gets the unit by map.
+     *
+     * @param mapId
+     *            the id of the map
+     * @return the unit by map
+     * @throws SQLException
+     *             the SQL exception
+     */
+    public HashMap<String, ArrayList<IVector>> getUnitByMap(final int mapId) throws SQLException {
+        final HashMap<String, ArrayList<IVector>> result = QueryDAO.getUnitByMap(mapId);
+        return result;
+    }
 
-	/**
-	 * Gets the unit by position.
-	 *
-	 * @param x
-	 *            the x coordinate
-	 * @param y
-	 *            the y coordinate
-	 * @param mapId
-	 *            the id of the map
-	 * @return the unit by position
-	 * @throws SQLException
-	 *             the SQL exception
-	 */
-	private String getUnitByPosition(final int x, final int y, final int mapId) throws SQLException {
-		return QueryDAO.getUnitByPosition(x, y, mapId);
-	}
+    /**
+     * Gets the unit by position.
+     *
+     * @param x
+     *            the x coordinate
+     * @param y
+     *            the y coordinate
+     * @param mapId
+     *            the id of the map
+     * @return the unit by position
+     * @throws SQLException
+     *             the SQL exception
+     */
+    private String getUnitByPosition(final int x, final int y, final int mapId)
+            throws SQLException {
+        return QueryDAO.getUnitByPosition(x, y, mapId);
+    }
 
-	/**
-	 * Gets the unit by type.
-	 *
-	 * @param type
-	 *            the type of the unit
-	 * @param mapId
-	 *            the id of the map
-	 * @return the unit by type
-	 * @throws SQLException
-	 *             the SQL exception
-	 */
-	private ArrayList<IVector> getUnitByType(final TypeEnum type, final int mapId) throws SQLException {
-		return QueryDAO.getUnitByType(type, mapId);
-	}
+    /**
+     * Gets the unit by type.
+     *
+     * @param type
+     *            the type of the unit
+     * @param mapId
+     *            the id of the map
+     * @return the unit by type
+     * @throws SQLException
+     *             the SQL exception
+     */
+    private ArrayList<IVector> getUnitByType(final TypeEnum type, final int mapId)
+            throws SQLException {
+        return QueryDAO.getUnitByType(type, mapId);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see modelInterfaces.IModel#loadLevel(int)
-	 */
-	@Override
-	public void loadLevel(final int levelId) throws SQLException {
-		final HashMap<String, ArrayList<IVector>> resultMap = QueryDAO.getUnitByMap(levelId);
+    /*
+     * (non-Javadoc)
+     *
+     * @see modelInterfaces.IModel#loadLevel(int)
+     */
+    @Override
+    public void loadLevel(final int levelId) throws SQLException {
+        final HashMap<String, ArrayList<IVector>> resultMap = QueryDAO.getUnitByMap(levelId);
 
-		final ILevel level = new Level(levelId, QueryDAO.getMap(levelId));
-		this.setLevel(level);
+        final ILevel level = new Level(levelId, QueryDAO.getMap(levelId));
+        this.setLevel(level);
 
-		String key;
-		ArrayList<IVector> position;
-		IEntity entity;
+        String key;
+        ArrayList<IVector> position;
+        IEntity entity;
 
-		for (final Entry<String, ArrayList<IVector>> result : resultMap.entrySet()) {
-			key = result.getKey();
-			position = result.getValue();	
-			
-			for(IVector vector : position) {
-				switch (key) {
-					case "WALL":
-							level.addUnit(
-							UnitFactory.createWall(WallType.WALL_ROUND,
-									QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
-							vector.getX(), vector.getY());
-							break;
-					case "WALL_H":
-							level.addUnit(
-							UnitFactory.createWall(WallType.WALL_HORIZONTAL,
-									QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
-							vector.getX(), vector.getY());
-							break;
-					case "WALL_V":
-							level.addUnit(
-							UnitFactory.createWall(WallType.WALL_VERTICAL,
-									QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
-							vector.getX(), vector.getY());
-							break;
-				default:
-					entity = UnitFactory.createEntity(TypeEnum.valueOf(key), QueryDAO.getSpritePath(TypeEnum.valueOf(key)));
-					entity.setPosition(vector);
-					level.addEntity(entity);
-					if (TypeEnum.valueOf(key) == TypeEnum.PLAYER) {
-						this.getLevel().setPlayer(entity);
-					} else if (TypeEnum.valueOf(key) == TypeEnum.DOOR_CLOSED) {
-						this.getLevel().setExit(entity);
-					}
-					break;
-				}
-			}
-		}
-		this.fillVoidSquares();
-		this.update();
+        for (final Entry<String, ArrayList<IVector>> result : resultMap.entrySet()) {
+            key = result.getKey();
+            position = result.getValue();
 
-	}
+            for (final IVector vector : position) {
+                switch (key) {
+                case "WALL":
+                    level.addUnit(
+                            UnitFactory.createWall(WallType.WALL_ROUND,
+                                    QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
+                            vector.getX(), vector.getY());
+                    break;
+                case "WALL_H":
+                    level.addUnit(
+                            UnitFactory.createWall(WallType.WALL_HORIZONTAL,
+                                    QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
+                            vector.getX(), vector.getY());
+                    break;
+                case "WALL_V":
+                    level.addUnit(
+                            UnitFactory.createWall(WallType.WALL_VERTICAL,
+                                    QueryDAO.getSpritePath(TypeEnum.valueOf(key)).get(0)),
+                            vector.getX(), vector.getY());
+                    break;
+                default:
+                    entity = UnitFactory.createEntity(TypeEnum.valueOf(key),
+                            QueryDAO.getSpritePath(TypeEnum.valueOf(key)));
+                    entity.setPosition(vector);
+                    level.addEntity(entity);
+                    if (TypeEnum.valueOf(key) == TypeEnum.PLAYER) {
+                        this.getLevel().setPlayer(entity);
+                    } else if (TypeEnum.valueOf(key) == TypeEnum.DOOR_CLOSED) {
+                        this.getLevel().setExit(entity);
+                    }
+                    break;
+                }
+            }
+        }
+        this.fillVoidSquares();
+        this.update();
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see modelInterfaces.IModel#resetScore()
-	 */
-	@Override
-	public void resetScore() {
-		this.getScore().resetScoreValue();
-	}
+    }
 
-	/**
-	 * @param level
-	 *            the level to set
-	 */
-	private void setLevel(final ILevel level) {
-		this.level = level;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see modelInterfaces.IModel#resetScore()
+     */
+    @Override
+    public void resetScore() {
+        this.getScore().resetScoreValue();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see modelInterfaces.IModel#setPlayerDirection(enums.DirectionEnum)
-	 */
-	@Override
-	public void setPlayerDirection(final DirectionEnum direction) {
+    /**
+     * @param level
+     *            the level to set
+     */
+    private void setLevel(final ILevel level) {
+        this.level = level;
+    }
 
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see modelInterfaces.IModel#setPlayerDirection(enums.DirectionEnum)
+     */
+    @Override
+    public void setPlayerDirection(final DirectionEnum direction) {
+        this.getPlayer().setDirection(VectorFactory.getVectorForDirection(direction));
+    }
 
-	/**
-	 * Sets the score
-	 *
-	 * @param score
-	 */
-	private void setScore(final Score score) {
-		this.score = score;
-	}
+    /**
+     * Sets the score
+     *
+     * @param score
+     */
+    private void setScore(final Score score) {
+        this.score = score;
+    }
 
-	@Override
-	public void update() {
-		this.setChanged();
-		this.notifyObservers();
-	}
+    @Override
+    public void update() {
+        this.setChanged();
+        this.notifyObservers();
+    }
 }
