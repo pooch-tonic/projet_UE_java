3138fa64f542054206f417dd186d9041f8b79863
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-06-05 14:49:56 +0200

    commit

--- Lorann/contract/src/main/java/modelInterfaces/IDodgeStrategy.java
+++ Lorann/contract/src/main/java/modelInterfaces/IDodgeStrategy.java
@@ -1,9 +1,20 @@
 package modelInterfaces;
 
+/**
+ * <h1>The IDodgeStrategy interface.</h1> Part of a strategy pattern.
+ *
+ * @author Ryo SHIINA
+ *
+ */
 public interface IDodgeStrategy {
 
 	/**
-	 * strategy pattern method for unit behavior on dodge
+	 * Strategy pattern method for unit behavior on dodge.
+	 * 
+	 * @param actor
+	 *            the entity executing the action
+	 * @param level
+	 *            the level containing the actor entity
 	 */
 	public void dodge(final IEntity actor, final ILevel level);
 
--- Lorann/contract/src/main/java/modelInterfaces/IModel.java
+++ Lorann/contract/src/main/java/modelInterfaces/IModel.java
@@ -1,6 +1,3 @@
-/*
- *
- */
 package modelInterfaces;
 
 import java.awt.Dimension;
@@ -17,115 +14,124 @@ import enums.TypeEnum;
  */
 public interface IModel {
 
-    public IEntity addEntityToLevel(TypeEnum type);
+	/**
+	 * Adds an entity to the level object.
+	 * 
+	 * @param type
+	 *            the type of the entity to add
+	 * @return the entity created by the input parameter.
+	 *
+	 */
+	public IEntity addEntityToLevel(TypeEnum type);
 
-    public void addEntityToLevel(IEntity entity);
+	public void addEntityToLevel(IEntity entity);
 
-    public void removeEntityFromLevel(final IEntity entity);
+	public void removeEntityFromLevel(final IEntity entity);
 
-    /**
-     * Adds a score value to the current score value
-     *
-     * @param value
-     */
-    void addToScore(final int value);
+	/**
+	 * Adds a score value to the current score value.
+	 *
+	 * @param value
+	 *            the value to add to the score
+	 */
+	void addToScore(final int value);
 
-    public void destroyEntity(IEntity entity);
+	public void destroyEntity(IEntity entity);
 
-    public IEntity getExit();
+	public IEntity getExit();
 
-    /**
-     * Gets a level
-     *
-     * @return a level
-     */
-    public ILevel getLevel();
+	/**
+	 * Gets a level
+	 *
+	 * @return a level
+	 */
+	public ILevel getLevel();
 
-    /**
-     * Gets the map.
-     *
-     * @param mapId
-     *            the id of the map
-     * @return the unit by position
-     * @throws SQLException
-     *             the SQL exception
-     */
-    Dimension getMap(int mapId) throws SQLException;
+	/**
+	 * Gets the map.
+	 *
+	 * @param mapId
+	 *            the id of the map
+	 * @return the unit by position
+	 * @throws SQLException
+	 *             the SQL exception
+	 */
+	Dimension getMap(int mapId) throws SQLException;
 
-    /**
-     * Gets the maxLevels
-     *
-     * @return the maxLevels
-     */
-    public int getMaxLevels();
+	/**
+	 * Gets the maxLevels
+	 *
+	 * @return the maxLevels
+	 */
+	public int getMaxLevels();
 
-    /**
-     * @return the player as an IEntity.
-     */
-    public IEntity getPlayer();
+	/**
+	 * @return the player as an IEntity.
+	 */
+	public IEntity getPlayer();
 
-    /**
-     * Gets the score
-     *
-     * @return the score
-     */
-    public IScore getScore();
+	/**
+	 * Gets the score
+	 *
+	 * @return the score
+	 */
+	public IScore getScore();
 
-    public IEntity getSpell();
+	public IEntity getSpell();
 
-    public IUnit getUnitOn(int x, int y);
+	public IUnit getUnitOn(int x, int y);
 
-    /**
-     * @param levelId
-     * @throws SQLException
-     */
-    void loadLevel(int levelId) throws SQLException;
+	/**
+	 * @param levelId
+	 * @throws SQLException
+	 */
+	void loadLevel(int levelId) throws SQLException;
 
-    /**
-     *
-     */
-    void resetScore();
+	/**
+	 *
+	 */
+	void resetScore();
 
-    /**
-     * Sets the maxLevels by reading the Database
-     */
-    public void setMaxLevels();
+	/**
+	 * Sets the maxLevels by reading the Database
+	 */
+	public void setMaxLevels();
 
-    public void setPlayer(IEntity player);
+	public void setPlayer(IEntity player);
 
-    /**
-     *
-     *
-     * @param direction
-     */
-    public void setPlayerDirection(DirectionEnum direction);
+	/**
+	 *
+	 *
+	 * @param direction
+	 */
+	public void setPlayerDirection(DirectionEnum direction);
 
-    /**
-     * Sets the player's spriteset current index to the specified value.
-     */
-    public void setPlayerSpriteSetToIndex(final int index);
+	/**
+	 * Sets the player's spriteset current index to the specified value.
+	 */
+	public void setPlayerSpriteSetToIndex(final int index);
 
-    /**
-     * Sets the player's spell attribute.
-     *
-     * @param spell
-     */
-    public void setSpell(IEntity spell);
+	/**
+	 * Sets the player's spell attribute.
+	 *
+	 * @param spell
+	 */
+	public void setSpell(IEntity spell);
 
-    /**
-     * updates the attributes of the entity.
-     */
-    public void update();
+	/**
+	 * updates the attributes of the entity.
+	 */
+	public void update();
 
-    /**
-     * Set the entity move strategy to the "do not move" one.
-     *
-     * @param entity
-     */
-    public void doNotMoveEntity(IEntity entity);
+	/**
+	 * Set the entity move strategy to the "do not move" one.
+	 *
+	 * @param entity
+	 */
+	public void doNotMoveEntity(IEntity entity);
 
-    /**
-     * Makes the spell bouncing towards the player.
-     */
-    public void callSpell();
+	/**
+	 * Makes the spell bouncing towards the player.
+	 */
+	public void callSpell();
 }
