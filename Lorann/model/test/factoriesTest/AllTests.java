caa87e63e95db6e9a2514a6428136de0e111b1c6
PtitCaillou <33094458+PtitCaillou@users.noreply.github.com>
2018-06-05 14:41:55 +0200

    UnitFactory tests done

---
+++ Lorann/model/test/factoriesTest/AllTests.java
@@ -0,0 +1,11 @@
+package factoriesTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UnitFactoryTest.class, VectorFactoryTest.class })
+public class AllTests {
+
+}
---
+++ Lorann/model/test/factoriesTest/UnitFactoryTest.java
@@ -0,0 +1,120 @@
+package factoriesTest;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Door;
+import model.Enemy;
+import model.Ground;
+import model.ImageLoader;
+import model.Key;
+import model.Loot;
+import model.Player;
+import model.Spell;
+import model.SpriteSet;
+import model.WallType;
+import model.factories.UnitFactory;
+import modelInterfaces.IUnit;
+
+public class UnitFactoryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateDoor_closed() throws IOException {
+		Door door = UnitFactory.createDoor_closed(new SpriteSet(ImageLoader.getImageByPath("/sprites/gate_closed.png")));
+		assertNotNull(door);
+	}
+
+	@Test
+	public void testCreateDoor_open() throws IOException {
+		Door door = UnitFactory.createDoor_open(new SpriteSet(ImageLoader.getImageByPath("/sprites/gate_open.png")));
+		assertNotNull(door);
+	}
+
+	@Test
+	public void testCreateEnemy_Arrbarr() throws IOException {
+		Enemy enemy = UnitFactory.createEnemy_Arrbarr(new SpriteSet(ImageLoader.getImageByPath("/sprites/monster_1.png")));
+		assertNotNull(enemy);
+	}
+
+	@Test
+	public void testCreateEnemy_Cargyv() throws IOException {
+		Enemy enemy = UnitFactory.createEnemy_Cargyv(new SpriteSet(ImageLoader.getImageByPath("/sprites/monster_2.png")));
+		assertNotNull(enemy);
+	}
+
+	@Test
+	public void testCreateEnemy_Kyracj() throws IOException {
+		Enemy enemy = UnitFactory.createEnemy_Kyracj(new SpriteSet(ImageLoader.getImageByPath("/sprites/monster_3.png")));
+		assertNotNull(enemy);
+	}
+
+	@Test
+	public void testCreateEnemy_Maarcg() throws IOException {
+		Enemy enemy = UnitFactory.createEnemy_Maarcg(new SpriteSet(ImageLoader.getImageByPath("/sprites/monster_4.png")));
+		assertNotNull(enemy);
+	}
+
+	@Test
+	public void testCreateGround_default() throws IOException {
+		Ground ground = UnitFactory.createGround_default(new SpriteSet(ImageLoader.getImageByPath("/sprites/gate_closed.png")));
+		assertNotNull(ground);
+	}
+
+	@Test
+	public void testCreateKey_blue() throws IOException {
+		Key key = UnitFactory.createKey_blue(new SpriteSet(ImageLoader.getImageByPath("/sprites/crystal_ball0.png")));
+		assertNotNull(key);
+	}
+
+	@Test
+	public void testCreateLoot_coin() throws IOException {
+		Loot loot = UnitFactory.createLoot_coin(new SpriteSet(ImageLoader.getImageByPath("../sprites/purse.png")));
+		assertNotNull(loot);
+	}
+
+	@Test
+	public void testCreatePlayer_Lorann() throws IOException {
+		Player player = UnitFactory.createPlayer_Lorann(new SpriteSet(ImageLoader.getImageByPath("/sprites/lorann_b.png")));
+		assertNotNull(player);
+	}
+
+	@Test
+	public void testCreateSpell() {
+		Spell spell = UnitFactory.createSpell();
+		assertNotNull(spell);
+	}
+
+	@Test
+	public void testCreateWall() {
+		IUnit wall = UnitFactory.createWall(WallType.WALL_ROUND, "/sprites/bone.png");
+		IUnit wall_h = UnitFactory.createWall(WallType.WALL_HORIZONTAL, "/sprites/horizontal_bone.png");
+		IUnit wall_v = UnitFactory.createWall(WallType.WALL_VERTICAL, "/sprites/vertical_bone.png");
+		assertNotNull(wall);
+		assertNotNull(wall_h);
+		assertNotNull(wall_v);
+	}
+}
