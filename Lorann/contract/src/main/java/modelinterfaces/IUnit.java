a07d76944f76adf6d79cb5cf00dbb9b9320a294a
Max Becerro <Max Becerro@DESKTOP-BEP008O>
2018-06-05 15:51:43 +0200

    Javadoc

--- Lorann/contract/src/main/java/modelinterfaces/IUnit.java
+++ Lorann/contract/src/main/java/modelinterfaces/IUnit.java
@@ -13,19 +13,45 @@ import vector.IVector;
 
 public interface IUnit extends ISquare {
 
+	/**
+	 * Gets a position
+	 * 
+	 * @return a position
+	 */
 	public IVector getPosition();
 
+	/**
+	 * @return a spriteSet
+	 */
 	public ISpriteSet getSpriteSet();
 
+	/**
+	 * @return
+	 */
 	public UnitTypeEnum getType();
 
+	/**
+	 * @param position
+	 */
 	public void setPosition(final IVector position);
 
+	/**
+	 * @param sprites
+	 */
 	public void setSpriteSet(final ArrayList<BufferedImage> sprites);
 
+	/**
+	 * @param image
+	 */
 	public void setSpriteSet(final BufferedImage image);
 
+	/**
+	 * @param spriteSet
+	 */
 	public void setSpriteSet(final ISpriteSet spriteSet);
 
+	/**
+	 * @param type
+	 */
 	public void setType(final UnitTypeEnum type);
 }
